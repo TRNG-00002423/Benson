de07298a35d704fac5973db4cae3962a4268aec5
Benson Zheng <bensonzheng2003@gmail.com>
2026-06-29 14:01:25 -0400

    implement transactions.sql

---
+++ assignments/week4/Tuesday/transactions_a.sql
@@ -0,0 +1,15 @@
+BEGIN;
+
+update product as p
+set stock_qty = p.stock_qty - t.total_qty
+from
+(select ol.product_id, ol.qty as total_qty from order_line as ol
+inner join order_header as oh
+on ol.order_id = oh.order_id
+where oh.status = 'PAID') as t
+where p.product_id = t.product_id 
+and p.stock_qty > t.total_qty;
+
+COMMIT;
+
+
---
+++ assignments/week4/Tuesday/transactions_c.sql
@@ -0,0 +1,16 @@
+BEGIN;
+
+SAVEPOINT after_header;
+
+update product as p
+set stock_qty = p.stock_qty - t.total_qty
+from
+(select ol.product_id, ol.qty as total_qty from order_line as ol
+inner join order_header as oh
+on ol.order_id = oh.order_id
+where oh.status = 'PAID') as t
+where p.product_id = t.product_id;
+
+SAVEPOINT after_lines;
+
+ROLLBACK TO after_header;
